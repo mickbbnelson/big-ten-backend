9c3bb662c4ce68db973fb901e1ec269e993f026b
mickbbnelson <75447451+mickbbnelson@users.noreply.github.com>
2022-02-27 16:26:59 -0600

    Add colleges controller actions

--- app/controllers/colleges_controller.rb
+++ app/controllers/colleges_controller.rb
@@ -1,2 +1,27 @@
 class CollegesController < ApplicationController
+
+    def index
+        college = College.all.order(:id)
+        render json: colleges
+    end
+
+    def create
+        college = College.new(college_params)
+        if college.save
+            render json: college
+        else
+            render json: {error: 'Failed to add'}
+        end
+    end
+
+    def destroy
+        college = College.find_by_id(params[:id])
+        college.destroy
+        render json: college.id
+    end
+
+    def college_params
+        params.permit(:name, :city, :state, :notes, :mascot, division_id)
+    end
+
 end
